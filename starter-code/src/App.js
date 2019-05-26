7996f846c3c6e0354c0c1a4a9f89317b89efa9ce
Antonio Méndez <antonio.mendez.martin89@gmail.com>
2019-05-26 23:07:06 +0200

    2nd iteration done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import foods from './data/foods.json'
+import FoodBox from './components/FoodBox'
+import FoodList from './components/FoodList'
+
 
 class App extends Component {
+  state ={
+    foods: foods.map(item => item)
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <FoodList foods ={this.state.foods}/>
       </div>
+      
     );
   }
 }
